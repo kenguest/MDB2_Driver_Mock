56278ec7ab9b1a4734950b73e6bc44a9fec2d834
Ken Guest <ken@linux.ie>
2013-03-31 23:38:21 +0100

    Add datatype class

---
+++ MDB2/Driver/Datatype/mock.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * mock.php
+ * 05-Feb-2013
+ *
+ * PHP Version 5
+ *
+ * @category Database
+ * @package  MDB2
+ * @author   Ken Guest <ken@linux.ie>
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link     mock.php
+ */
+require_once 'MDB2/Driver/Datatype/Common.php';
+
+/**
+ * MDB2 Mock driver
+ *
+ * @category Database
+ * @package  MDB2
+ * @author   Ken Guest <ken@linux.ie>
+ * @license  BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link     mock.php
+ */
+class MDB2_Driver_Datatype_mock extends MDB2_Driver_Datatype_Common
+{
+}
+?>
